32b57bd2adf5bc2b80f5c9d80ec4e8b8b5de390c
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2020-02-20 17:17:03 -0500

    v1.0.2 to support ENC28J60 and UIPEThernet library
    ### Version v1.0.2
    
    1. From v1.0.2+, the library supports many more Arduino boards (Atmel AVR-s, Atmel SAM3X8E ARM Cortex-M3, STM32F series, ESP8266, Intel ARC32(Genuino101), Nordic nRF51(RFduino), Teensy boards, Realtek Ameba(RTL8195A,RTL8710))
    2. Support Wiznet W5x00 or ENC28J60 EThernet shields by using [UIPEthernet](https://github.com/UIPEthernet/UIPEthernet) library besides standard Ethernet library.

--- src/detail/RequestHandler.h
+++ src/detail/RequestHandler.h
@@ -7,7 +7,7 @@
  * Forked and modified from ESP8266 https://github.com/esp8266/Arduino/releases
  * Built by Khoi Hoang https://github.com/khoih-prog/ESP8266_AT_WebServer
  * Licensed under MIT license
- * Version: 1.0.1
+ * Version: 1.0.2
  *
  * Original author:
  * @file       Esp8266WebServer.h
@@ -16,7 +16,8 @@
  * Version Modified By   Date      Comments
  * ------- -----------  ---------- -----------
  *  1.0.0   K Hoang      13/02/2020 Initial coding for Arduino Mega, Teensy, etc to support Ethernetx libraries
- *  1.0.1   K Hoang      20/02/2020 Add support to UIPEthernet library
+ *  1.0.1   K Hoang      20/02/2020 Add support to lambda functions
+ *  1.0.2   K Hoang      20/02/2020 Add support to UIPEthernet library for ENC28J60
  *****************************************************************************************************************************/
 
 #ifndef RequestHandler_h
