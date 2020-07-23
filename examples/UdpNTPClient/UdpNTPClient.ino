dd16add49c789f9db6fcc4800335f008a142b290
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2020-07-22 22:30:47 -0400

    Add UDP and NTP examples

---
+++ examples/UdpNTPClient/UdpNTPClient.ino
@@ -0,0 +1,164 @@
+/****************************************************************************************************************************
+    UdpNTPClient.ino - Simple Arduino web server sample for ESP8266 AT-command shield
+    EthernetWebServer is a library for the Ethernet shields to run WebServer
+
+    Based on and modified from ESP8266 https://github.com/esp8266/Arduino/releases
+    Built by Khoi Hoang https://github.com/khoih-prog/EthernetWebServer
+    Licensed under MIT license
+    Version: 1.0.10
+
+
+    Original author:
+    @file       Esp8266WebServer.h
+    @author     Ivan Grokhotkov
+
+   ersion Modified By   Date      Comments
+   ------- -----------  ---------- -----------
+    1.0.0   K Hoang      13/02/2020 Initial coding for Arduino Mega, Teensy, etc to support Ethernetx libraries
+    1.0.1   K Hoang      20/02/2020 Add support to lambda functions
+    1.0.2   K Hoang      20/02/2020 Add support to UIPEthernet library for ENC28J60
+    1.0.3   K Hoang      23/02/2020 Add support to SAM DUE / SAMD21 boards
+    1.0.4   K Hoang      16/04/2020 Add support to SAMD51 boards
+    1.0.5   K Hoang      24/04/2020 Add support to nRF52 boards, such as AdaFruit Feather nRF52832, nRF52840 Express, BlueFruit Sense,
+                                    Itsy-Bitsy nRF52840 Express, Metro nRF52840 Express, NINA_B30_ublox, etc.
+                                    More Custom Ethernet libraries supported such as Ethernet2, Ethernet3, EthernetLarge
+    1.0.6   K Hoang      27/04/2020 Add W5x00 support to ESP32/ESP8266 boards
+    1.0.7   K Hoang      30/04/2020 Add ENC28J60 support to ESP32/ESP8266 boards
+    1.0.8   K Hoang      12/05/2020 Fix W5x00 support for ESP8266 boards.
+    1.0.9   K Hoang      15/05/2020 Add EthernetWrapper.h for easier W5x00 support as well as more Ethernet libs in the future.
+    1.0.10  K Hoang      21/07/2020 Fix bug not closing client and releasing socket.
+ *****************************************************************************************************************************/
+
+#include "defines.h"
+
+char timeServer[]         = "time.nist.gov";  // NTP server
+unsigned int localPort    = 2390;             // local port to listen for UDP packets
+
+const int NTP_PACKET_SIZE = 48;       // NTP timestamp is in the first 48 bytes of the message
+const int UDP_TIMEOUT     = 2000;     // timeout in miliseconds to wait for an UDP packet to arrive
+
+byte packetBuffer[NTP_PACKET_SIZE];   // buffer to hold incoming and outgoing packets
+
+// A UDP instance to let us send and receive packets over UDP
+EthernetUDP Udp;
+
+// send an NTP request to the time server at the given address
+void sendNTPpacket(char *ntpSrv)
+{
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  Udp.beginPacket(ntpSrv, 123); //NTP requests are to port 123
+
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+
+  Udp.endPacket();
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial);
+
+#ifdef ETHERNET_NAME
+  Serial.println("\nStart UdpNTPClient on " + String(BOARD_NAME) + " board, running " + String(ETHERNET_NAME));
+#else
+  Serial.println("\nStart UdpNTPClient on " + String(BOARD_NAME));
+#endif
+
+  // start the ethernet connection and the server
+  // Use random mac
+  srand(1);
+  uint16_t index = rand() % NUMBER_OF_MAC;
+  //uint16_t index = random(NUMBER_OF_MAC);
+
+  // Use Static IP
+  //Ethernet.begin(mac[index], ip);
+  // Use DHCP dynamic IP and random mac
+  Ethernet.begin(mac[index]);
+
+  // you're connected now, so print out the data
+  Serial.print(F("You're connected to the network, IP = "));
+  Serial.println(Ethernet.localIP());
+
+  Udp.begin(localPort);
+}
+
+void loop()
+{
+  sendNTPpacket(timeServer); // send an NTP packet to a time server
+
+  // wait for a reply for UDP_TIMEOUT miliseconds
+  unsigned long startMs = millis();
+  while (!Udp.available() && (millis() - startMs) < UDP_TIMEOUT) {}
+
+  // if there's data available, read a packet
+  int packetSize = Udp.parsePacket();
+
+  if (packetSize)
+  {
+    Serial.print(F("UDP Packet received, size "));
+    Serial.println(packetSize);
+    Serial.print(F("From "));
+    IPAddress remoteIp = Udp.remoteIP();
+    Serial.print(remoteIp);
+    Serial.print(F(", port "));
+    Serial.println(Udp.remotePort());
+
+    // We've received a packet, read the data from it into the buffer
+    Udp.read(packetBuffer, NTP_PACKET_SIZE);
+
+    // the timestamp starts at byte 40 of the received packet and is four bytes,
+    // or two words, long. First, esxtract the two words:
+
+    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+
+    // combine the four bytes (two words) into a long integer
+    // this is NTP time (seconds since Jan 1 1900):
+    unsigned long secsSince1900 = highWord << 16 | lowWord;
+    Serial.print(F("Seconds since Jan 1 1900 = "));
+    Serial.println(secsSince1900);
+
+    // now convert NTP time into )everyday time:
+    Serial.print(F("Unix time = "));
+    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+    const unsigned long seventyYears = 2208988800UL;
+    // subtract seventy years:
+    unsigned long epoch = secsSince1900 - seventyYears;
+    // print Unix time:
+    Serial.println(epoch);
+
+    // print the hour, minute and second:
+    Serial.print(F("The UTC time is "));       // UTC is the time at Greenwich Meridian (GMT)
+    Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
+    Serial.print(F(":"));
+    if (((epoch % 3600) / 60) < 10) {
+      // In the first 10 minutes of each hour, we'll want a leading '0'
+      Serial.print(F("0"));
+    }
+    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
+    Serial.print(F(":"));
+    if ((epoch % 60) < 10) {
+      // In the first 10 seconds of each minute, we'll want a leading '0'
+      Serial.print(F("0"));
+    }
+    Serial.println(epoch % 60); // print the second
+  }
+  // wait ten seconds before asking for the time again
+  delay(10000);
+}
---
+++ examples/UdpSendReceive/UdpSendReceive.ino
@@ -0,0 +1,106 @@
+/****************************************************************************************************************************
+    UDPSendReceive.ino - Simple Arduino web server sample for ESP8266/ESP32 AT-command shield
+    
+    EthernetWebServer is a library for the Ethernet shields to run WebServer
+
+    Based on and modified from ESP8266 https://github.com/esp8266/Arduino/releases
+    Built by Khoi Hoang https://github.com/khoih-prog/EthernetWebServer
+    Licensed under MIT license
+    Version: 1.0.10
+
+    Original author:
+    @file       Esp8266WebServer.h
+    @author     Ivan Grokhotkov
+
+   ersion Modified By   Date      Comments
+   ------- -----------  ---------- -----------
+    1.0.0   K Hoang      13/02/2020 Initial coding for Arduino Mega, Teensy, etc to support Ethernetx libraries
+    1.0.1   K Hoang      20/02/2020 Add support to lambda functions
+    1.0.2   K Hoang      20/02/2020 Add support to UIPEthernet library for ENC28J60
+    1.0.3   K Hoang      23/02/2020 Add support to SAM DUE / SAMD21 boards
+    1.0.4   K Hoang      16/04/2020 Add support to SAMD51 boards
+    1.0.5   K Hoang      24/04/2020 Add support to nRF52 boards, such as AdaFruit Feather nRF52832, nRF52840 Express, BlueFruit Sense,
+                                    Itsy-Bitsy nRF52840 Express, Metro nRF52840 Express, NINA_B30_ublox, etc.
+                                    More Custom Ethernet libraries supported such as Ethernet2, Ethernet3, EthernetLarge
+    1.0.6   K Hoang      27/04/2020 Add W5x00 support to ESP32/ESP8266 boards
+    1.0.7   K Hoang      30/04/2020 Add ENC28J60 support to ESP32/ESP8266 boards
+    1.0.8   K Hoang      12/05/2020 Fix W5x00 support for ESP8266 boards.
+    1.0.9   K Hoang      15/05/2020 Add EthernetWrapper.h for easier W5x00 support as well as more Ethernet libs in the future.
+    1.0.10  K Hoang      21/07/2020 Fix bug not closing client and releasing socket.
+ *****************************************************************************************************************************/
+
+#include "defines.h"
+
+unsigned int localPort = 1883;    //10002;  // local port to listen on
+
+char packetBuffer[255];          // buffer to hold incoming packet
+char ReplyBuffer[] = "ACK";      // a string to send back
+
+// A UDP instance to let us send and receive packets over UDP
+EthernetUDP Udp;
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial);
+
+#ifdef ETHERNET_NAME
+  Serial.println("\nStart UDPSendReceive on " + String(BOARD_NAME) + " board, running " + String(ETHERNET_NAME));
+#else
+  Serial.println("\nStart UDPSendReceive on " + String(BOARD_NAME));
+#endif  
+
+  // start the ethernet connection and the server
+  // Use random mac
+  srand(1);
+  uint16_t index = rand() % NUMBER_OF_MAC;
+  //uint16_t index = random(NUMBER_OF_MAC);
+
+  // Use Static IP
+  //Ethernet.begin(mac[index], ip);
+  // Use DHCP dynamic IP and random mac
+  Ethernet.begin(mac[index]);
+
+  // you're connected now, so print out the data
+  Serial.print(F("You're connected to the network, IP = "));
+  Serial.println(Ethernet.localIP());
+
+  Serial.println(F("\nStarting connection to server..."));
+  // if you get a connection, report back via serial:
+  Udp.begin(localPort);
+
+  Serial.print(F("Listening on port "));
+  Serial.println(localPort);
+}
+
+void loop()
+{
+  // if there's data available, read a packet
+  int packetSize = Udp.parsePacket();
+
+  if (packetSize)
+  {
+    Serial.print(F("Received packet of size "));
+    Serial.println(packetSize);
+    Serial.print(F("From "));
+    IPAddress remoteIp = Udp.remoteIP();
+    Serial.print(remoteIp);
+    Serial.print(F(", port "));
+    Serial.println(Udp.remotePort());
+
+    // read the packet into packetBufffer
+    int len = Udp.read(packetBuffer, 255);
+    if (len > 0)
+    {
+      packetBuffer[len] = 0;
+    }
+
+    Serial.println(F("Contents:"));
+    Serial.println(packetBuffer);
+
+    // send a reply, to the IP address and port that sent us the packet we received
+    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    Udp.write(ReplyBuffer);
+    Udp.endPacket();
+  }
+}
