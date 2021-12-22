b7b8ea6859eac5a3c71cccb58a2cd6d7f98571ec
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2021-12-21 23:33:28 -0500

    Update Teensy Packages_Patches
    Tested with Arduino v1.8.19 and Teensyduino v1.56-b6

--- Packages_Patches/hardware/teensy/avr/cores/teensy/Stream.h
+++ Packages_Patches/hardware/teensy/avr/cores/teensy/Stream.h
@@ -26,48 +26,124 @@
 class Stream : public Print
 {
   public:
-	Stream() : _timeout(1000), read_error(0) {}
-	virtual int available() = 0;
-	virtual int read() = 0;
-	virtual int peek() = 0;
-	virtual void flush() = 0;
+    constexpr Stream() : _timeout(1000), read_error(0) {}
+    virtual int available() = 0;
+    virtual int read() = 0;
+    virtual int peek() = 0;
 
-	void setTimeout(unsigned long timeout);
-	bool find(const char *target);
-	bool find(const uint8_t *target) { return find ((const char *)target); }
-	bool find(const char *target, size_t length);
-	bool find(const uint8_t *target, size_t length) { return find ((const char *)target, length); }
-	bool findUntil(const char *target, char *terminator);
-	bool findUntil(const uint8_t *target, char *terminator) { return findUntil((const char *)target, terminator); }
-	bool findUntil(const char *target, size_t targetLen, char *terminate, size_t termLen);
-	bool findUntil(const uint8_t *target, size_t targetLen, char *terminate, size_t termLen) {return findUntil((const char *)target, targetLen, terminate, termLen); }
-	long parseInt();
-	long parseInt(char skipChar);
-	float parseFloat();
-	float parseFloat(char skipChar);
-	size_t readBytes(char *buffer, size_t length);
-	size_t readBytes( uint8_t *buffer, size_t length) { return readBytes((char *)buffer, length); }
-	size_t readBytesUntil(char terminator, char *buffer, size_t length);
-	size_t readBytesUntil(char terminator, uint8_t *buffer, size_t length) { return readBytesUntil(terminator, (char *)buffer, length); }
-	String readString(size_t max = 120);
-	String readStringUntil(char terminator, size_t max = 120);
-	int getReadError() { return read_error; }
-	void clearReadError() { setReadError(0); }
+    void setTimeout(unsigned long timeout);
+    bool find(const char *target);
+    
+    bool find(const uint8_t *target) 
+    {
+      return find ((const char *)target);
+    }
+    
+    bool find(const String &target) 
+    {
+      return find(target.c_str());
+    }
+    
+    bool find(const char *target, size_t length);
+    
+    bool find(const uint8_t *target, size_t length) 
+    {
+      return find ((const char *)target, length);
+    }
+    
+    bool find(const String &target, size_t length) 
+    {
+      return find(target.c_str(), length);
+    }
+    
+    bool findUntil(const char *target, const char *terminator);
+    
+    bool findUntil(const uint8_t *target, const char *terminator) 
+    {
+      return findUntil((const char *)target, terminator);
+    }
+    
+    bool findUntil(const String &target, const char *terminator) 
+    {
+      return findUntil(target.c_str(), terminator);
+    }
+    
+    bool findUntil(const char *target, const String &terminator) 
+    {
+      return findUntil(target, terminator.c_str());
+    }
+    
+    bool findUntil(const String &target, const String &terminator) 
+    {
+      return findUntil(target.c_str(), terminator.c_str());
+    }
+    
+    bool findUntil(const char *target, size_t targetLen, const char *terminate, size_t termLen);
+    
+    bool findUntil(const uint8_t *target, size_t targetLen, const char *terminate, size_t termLen) 
+    {
+      return findUntil((const char *)target, targetLen, terminate, termLen);
+    }
+    
+    bool findUntil(const String &target, size_t targetLen, const char *terminate, size_t termLen);
+    bool findUntil(const char *target, size_t targetLen, const String &terminate, size_t termLen);
+    bool findUntil(const String &target, size_t targetLen, const String &terminate, size_t termLen);
+    
+    long parseInt();
+    long parseInt(char skipChar);
+    
+    float parseFloat();
+    float parseFloat(char skipChar);
+    
+    size_t readBytes(char *buffer, size_t length);
+    
+    size_t readBytes(uint8_t *buffer, size_t length) 
+    {
+      return readBytes((char *)buffer, length);
+    }
+    
+    size_t readBytesUntil(char terminator, char *buffer, size_t length);
+    
+    size_t readBytesUntil(char terminator, uint8_t *buffer, size_t length) 
+    {
+      return readBytesUntil(terminator, (char *)buffer, length);
+    }
+
+    ////////////////////////////////////////////////////////////
+    String readString(size_t max = 512);
+    String readStringUntil(char terminator, size_t max = 512);
+
+    // KH, to not use String
+    char* readCharsUntil(char terminator, size_t max = 512);
+    ////////////////////////////////////////////////////////////
+
+    int getReadError() 
+    {
+      return read_error;
+    }
+    
+    void clearReadError() 
+    {
+      setReadError(0);
+    }
+    
   protected:
-	void setReadError(int err = 1) { read_error = err; }
-	unsigned long _timeout;
-	
-  // KH
-	int timedRead();
-	//////
-	
+    void setReadError(int err = 1) 
+    {
+      read_error = err;
+    }
+    
+    unsigned long _timeout;
+
+    // KH
+    int timedRead();
+    int timedPeek();
+    int peekNextDigit();
+    //////
+
   private:
-	char read_error;
-	
-	//int timedRead();
-	
-	int timedPeek();
-	int peekNextDigit();
+    char read_error;
 };
 
-#endif	
+#endif
+
